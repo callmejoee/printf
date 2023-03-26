8036be2f059c8d9bfac0d4217d214c95c5d81b1f
Youssef <youssefkaramedu@gmail.com>
2023-03-26 12:04:01 -0700

    Remove printf

--- test.c
+++ test.c
@@ -9,10 +9,14 @@
 
 int main(void)
 {
-	int len;
-	len = _printf("%c", 'm');
-	printf(" <- letter m is printed and length is :%d\n", len);
-	len = _printf("Character:[%c]\n number is ", 'H');
-	printf("%d", len);
+	int len, len2;
+	
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+   	printf("Character:[%c]\n", 'H');
+	printf("Length 1 comes to 39 and is : %d\n and length two is %d\n", len, len2);
+
+
 	return (0);
 }
