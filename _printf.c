0be67624f102306d5a827b420cdd41232b54bf6c
Youssef <youssefkaramedu@gmail.com>
2023-03-27 04:06:36 -0700

    Add file to print string

--- _printf.c
+++ _printf.c
@@ -10,43 +10,37 @@
 
 int _printf(const char *format, ...)
 {
-        int char_count = 0;
-        int i = 0;
-        char *str;
-        int j = 0;
-        va_list ptr;
+	int char_count = 0;
+	int i = 0;
+	char *str;
+	va_list ptr;
 
-        va_start(ptr, format);
+	va_start(ptr, format);
 
-        while (format[i] != '\0' && format != NULL)
-        {
-                if (format[i] == '%')
-                {
-                        ++i;
-                        switch (format[i])
-                        {
-                                case 'c':
-                                        ++char_count;
-                                        _putchar(va_arg(ptr, int));
-                                        break;
-                                case 's':
-                                        str = va_arg(ptr, char *);
-                                        while (str != NULL && str[j] != '\0')
-                                        {
-                                                ++char_count;
-                                                _putchar(str[j]);
-                                                j++;
-                                        }
-                                        break;
-                        }
-                }
-                else
-                {
-                        ++char_count;
-                        _putchar(format[i]);
-                }
-                i++;
-        }
+	while (format[i] != '\0' && format != NULL)
+	{
+		if (format[i] == '%')
+		{
+			++i;
+			switch (format[i])
+			{
+				case 'c':
+					++char_count;
+					_putchar(va_arg(ptr, int));
+					break;
+				case 's':
+					str = va_arg(ptr, char *);
+					char_count += print_str(str);
+					break;
+			}
+		}
+		else
+		{
+			++char_count;
+			_putchar(format[i]);
+		}
+		i++;
+	}
 
-        return (char_count);
+	return (char_count);
 }
---
+++ print_str.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** this is a function to prints strings
+ *
+ * @str: string to be printed
+ *
+ * Return: num of chars printd
+ */
+
+int print_str(char *str)
+{
+	int j = 0;
+	int count = 0;
+	if (str == NULL)
+		str = "(null)";
+	while (str != NULL && str[j] != '\0')
+	{
+		++count;
+		_putchar(str[j]);
+		j++;
+	}
+	return (count);
+}
