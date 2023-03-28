34e8d22643e3a991c1f1a4adb0137738768e41ef
flowsar <brahim.sar151@gmail.com>
2023-03-28 18:40:49 +0000

    fix all betty style error

--- _putchar.c
+++ _putchar.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
  * _putchar - function to print letter
- *
  * @c: char
- *
  * Return: void
  *
  */
-
 void _putchar(char c)
 {
-    write (1, &c, 1);
+	write(1, &c, 1);
 }
