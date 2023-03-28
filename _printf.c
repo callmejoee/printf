5e1b7e69fecccd0c21fb38812ce164ca37f2354a
flowsar <brahim.sar151@gmail.com>
2023-03-28 20:41:15 +0000

    all function in one file

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
+#include <unistd.h>
 
 int printchar(char c);
 int _printf(const char *format, ...);
@@ -55,4 +55,69 @@ int _printf(const char *format, ...)
 	}
 	va_end(arg);
 	return (len);
+}
+
+/**
+ * printstring - function print string to stdou;
+ * @str: string
+ * Return: return length of str.
+ */
+int printstring(char *str)
+{
+	int len = 0, i = 0;
+
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+	{
+		len += printchar(str[i]);
+		i++;
+	}
+	return (len);
+}
+/**
+ * printchar - function print c to stdou;
+ * @c: character
+ * Return: return 1 length of char.
+ */
+
+int printchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int printchar(char c);
+/**
+ * print_num_helper- this function help print_num to print int;
+ * @x: number
+ * Return: retunr the length of int.
+ */
+int print_num_helper(int x)
+{
+	char c;
+	int i;
+
+	if (x == 0)
+		return (0);
+	i = 1 + print_num_helper(x / 10);
+	c = x % 10 + '0';
+	printchar(c);
+	return (i);
+}
+/**
+ * print_num - this function print number as a char;
+ * @x: number
+ * Return: return length of int.
+ */
+int print_num(int x)
+{
+	int length;
+
+	if (x < 0)
+	{
+		printchar('-');
+		length = print_num_helper(-x);
+		return (length);
+	}
+	length = print_num_helper(x);
+	return (length);
 }
