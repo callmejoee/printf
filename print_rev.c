af6244121138d1cae9ef342ffffcb89c6e95a288
Youssef <youssefkaramedu@gmail.com>
2023-03-28 14:53:17 -0700

    Add print reverse

---
+++ print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of the string
+ *
+ * @str: string
+ *
+ * Retrun: num of times function prints
+ *
+ */
+
+int print_rev(char * str)
+{
+	int len, i = 0, count = 0;
+
+	len = strlen(str)i - 1;
+
+	while (str[i] != '\0')
+	{
+		++count;
+		_putchar(str[len - i]);
+		i++;
+	}
+}
