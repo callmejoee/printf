301a049cbba2499b5e5cc2b0d5e6828ba1c7f000
Youssef <youssefkaramedu@gmail.com>
2023-03-28 15:13:50 -0700

    Add some changes

--- test_cases/test_reverse.c
+++ test_cases/test_reverse.c
@@ -1,30 +1,10 @@
 #include "../main.h"
+#include "../*c"
 
 int main(void)
-{
-       	// Test case 1: simple string
-    char str1[] = "hello";
-    printf("Test case 1:\nInput: \"%s\"\nExpected output: \"olleh\"\n", str1);
-    _printf("%r", str1);
-    printf("\n\n");
+{	
 
-    // Test case 2: empty string
-    char str2[] = "";
-    printf("Test case 2:\nInput: \"%s\"\nExpected output: \"\"\n", str2);
-    _printf("%r", str2);
-    printf("\n\n");
-
-    // Test case 3: string with one character
-    char str3[] = "a";
-    printf("Test case 3:\nInput: \"%s\"\nExpected output: \"a\"\n", str3);
-    _printf("%r", str3);
-    printf("\n\n");
-
-    // Test case 4: string with special characters
-    char str4[] = "Hello, world!";
-    printf("Test case 4:\nInput: \"%s\"\nExpected output: \"!dlrow ,olleH\"\n", str4);
-    _printf("%r", str4);
-    printf("\n\n");
+	_printf ("%r", "My name is Youssef");
 
 	return(0);
 }
